80211df587871c9a8c1bd67cdf2cd3f5de5e91f9
Laptop Kevnnard <londonokevin9@gmail.com>
2022-12-25 13:58:42 -0500

    Comienzo Web 3.0

--- components/dashboard/Camera.js
+++
@@ -1,8 +0,0 @@
-import { PerspectiveCamera } from "@react-three/drei";
-import React from "react";
-
-function Camera() {
-  return <PerspectiveCamera makeDefault position={[35, 18, 20]} fov={15} />;
-}
-
-export default Camera;
